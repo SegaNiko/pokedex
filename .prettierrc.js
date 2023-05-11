e687e4ba2435c4bb2cca52ebb336591cd678a985
SegaNiko <sergiynikolaiychuk@gmail.com>
2023-05-11 13:32:38 +0300

    tech: add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+};
