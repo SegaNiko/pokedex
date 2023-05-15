886eba0c12157405c06150111b10038733da9d9b
SegaNiko <sergiynikolaiychuk@gmail.com>
2023-05-15 13:22:52 +0300

    tech: update alias

--- craco.config.js
+++ craco.config.js
@@ -5,9 +5,14 @@ module.exports = {
     alias: {
       '@ui-kit': path.resolve(__dirname, 'src/ui-kit'),
       '@components': path.resolve(__dirname, 'src/components'),
-      '@scss': path.resolve(__dirname, 'src/scss'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@store': path.resolve(__dirname, 'src/store'),
       '@img': path.resolve(__dirname, 'src/assets/img'),
       '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+      '@consts': path.resolve(__dirname, 'src/consts'),
+      "@models": path.resolve(__dirname, 'src/models'),
+      "@utils": path.resolve (__dirname, './src/utils'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
       '@api': path.resolve(__dirname, 'src/api'),
     }
   },
