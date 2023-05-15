c3cefa26365f64cd580f22c4a2862899c74f2560
SegaNiko <sergiynikolaiychuk@gmail.com>
2023-05-15 13:30:40 +0300

    feat: add Modal

---
+++ src/ui-kit/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ModalWrapper, ModalContent, CloseButton, ModalCloseWrap } from './styles';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const handleClose = (e: React.MouseEvent) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  return (
+    <ModalWrapper isOpen={isOpen}>
+      <ModalCloseWrap onClick={handleClose} />
+      <ModalContent>
+        <CloseButton onClick={handleClose}>X</CloseButton>
+        {children}
+      </ModalContent>
+    </ModalWrapper>
+  );
+};
+
+export default Modal;
