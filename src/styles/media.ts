e065a7f35d688b559d112c4f2418109d0d10e0eb
SegaNiko <sergiynikolaiychuk@gmail.com>
2023-05-15 13:26:16 +0300

    feat: add styles folder

---
+++ src/styles/media.ts
@@ -0,0 +1,19 @@
+const size = {
+  mobileXS: '320px',
+  mobileS: '375px',
+  mobileM: '425px',
+  mobileL: '768px',
+  mediaL: '1025px',
+  mediaXL: '1440px',
+  mediaXXL: '1920px'
+}
+
+export const devices = {
+  mobileXS: `(max-width:  ${size.mobileXS})`,
+  mobileS: `(max-width:  ${size.mobileS})`,
+  mobileM: `(max-width:  ${size.mobileM})`,
+  mobileL: `(max-width:  ${size.mobileL})`,
+  mediaL: `(max-width:  ${size.mediaL})`,
+  mediaXL: `(max-width:  ${size.mediaXL})`,
+  mediaXXL: `(max-width:  ${size.mediaXXL})`,
+};
